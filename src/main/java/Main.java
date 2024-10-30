e6e189db4c25c956c8daecadb4d890719436363a
106010133 <avakankirill@gmail.com>
2024-10-30 11:02:10 +0300

    Main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main branch");
     }
+
+    public static int getNum() {
+        int a = 1;
+        int b = 2;
+        return a + b;
+    }
 }
